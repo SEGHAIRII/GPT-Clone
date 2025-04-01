4e6f04a16406c8877253eb9106d2210f0cf1b38f
SEGHAIRII <la_seghairi@esi.dz>
2025-04-01 21:57:32 +0100

    added the gpt dataset with sliding window and the corresponding dataloader

---
+++ GPT/dataset/dataset.py
@@ -0,0 +1,30 @@
+import torch
+from torch.utils.data import Dataset, DataLoader
+import tiktoken
+
+class gptDataSet(Dataset):
+    def __init(self, text:str, tokenizer:tiktoken.core.Encoding, context_size: int, stride:int):
+        self.tokenizer = tokenizer
+        self.context_size = context_size
+        self.stride = stride
+        self.inputs = []
+        self.outputs = []
+        tokens = tokenizer.encode(text)
+        for i in range(0, len(tokens) - context_size, stride):
+            input_tokens = tokens[i:i + context_size ]
+            output_tokens = tokens[i + 1:i + context_size + 1]
+            self.inputs.append(torch.as_tensor(input_tokens))
+            self.outputs.append(torch.as_tensor(output_tokens))
+            
+    def __len__(self):
+        return len(self.inputs)
+    def __getitem__(self, idx):
+        return self.inputs[idx], self.outputs[idx]
+    
+    
+    
+def get_dataloder(text:str, tokenizer:tiktoken.core.Encoding, context_size:int, stride:int, batch_size:int, shuffle:bool=True, num_workers:int=0):
+    dataset = gptDataSet(text, tokenizer, context_size, stride)
+    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=shuffle, num_workers=num_workers)
+    return dataloader
+
