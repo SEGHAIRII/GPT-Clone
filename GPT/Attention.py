9aa38c0d459ae0c9d1c7e63b8a1077d326b6f699
SEGHAIRII <la_seghairi@esi.dz>
2025-04-09 20:10:22 +0100

    implemented causal attention

--- GPT/Attention.py
+++ GPT/Attention.py
@@ -2,25 +2,33 @@ import torch.nn as nn
 import torch
 
 
-class Attention(nn.Module):
-    def __init__(self, d_in:int, d_out:int):
-        super(Attention, self).__init__()
+class MaskedAttention(nn.Module):
+    def __init__(self, d_in:int, d_out:int, context_length:int, dropout:float=0.0):
+        super().__init__()
+        self.d_out = d_out
         self.W_keys = nn.Linear(d_in, d_out, bias=False)
         self.W_queries = nn.Linear(d_in, d_out, bias=False)
         self.W_values = nn.Linear(d_in, d_out, bias=False)
+        self.dropout = nn.Dropout(dropout)
+        self.register_buffer(
+            'mask', torch.triu(torch.ones(context_length, context_length), diagonal=-1)
+        )
+        
         
     def forward(self, X):
+        b, num_tokens, d_in = X.size()
         keys = self.W_keys(X)
         queries = self.W_queries(X)
         values = self.W_values(X)
         
-        scores = torch.matmul(queries, keys.T) / (keys.size(-1) ** 0.5)
+        scores = torch.matmul(queries, keys.transpose(1,2))
         
-        attn_weights = nn.functional.softmax(scores, dim=-1)
-        
+        scores.masked_fill_(self.mask[:num_tokens, :num_tokens] == 0, - torch.inf)
+        attn_weights = torch.softmax(scores / keys.shape[-1] ** 0.5, dim=-1)
         context_vect = torch.matmul(attn_weights, values)
         
         return context_vect
+    
     
     
     
