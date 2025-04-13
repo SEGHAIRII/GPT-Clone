c973ac64a794674c2fc04d9fa8bd952eab56f9ca
SEGHAIRII <la_seghairi@esi.dz>
2025-04-13 18:32:11 +0100

    implemented the transformer block

---
+++ GPT/SubModules.py
@@ -0,0 +1,39 @@
+import torch
+import torch.nn as nn
+
+class LayerNorm(nn.Module):
+    def __init__(self, dim: int, eps: float = 1e-5):
+        super().__init__()
+        self.eps = eps
+        self.dim = dim
+        self.scale = nn.Parameter(torch.ones(dim))
+        self.shift = nn.Parameter(torch.zeros(dim))
+
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        mean = x.mean(dim=-1, keepdim=True)
+        var = x.var(dim=-1, keepdim=True, unbiased=False)
+        norm_x = (x - mean) / torch.sqrt(var + self.eps)
+        return self.scale * norm_x + self.shift
+    
+    
+class GelU(nn.Module):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return 0.5 * x * (1 + torch.tanh(torch.sqrt(2 / torch.pi) * (x + 0.044715 * x ** 3)))
+    
+    
+class Linear(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+        self.layers = nn.Sequential(
+            nn.Linear(cfg['emb_dim'], 4 * cfg['emb_dim']),
+            GelU(),
+            nn.Linear(4 * cfg['emb_dim'], cfg['emb_dim']),
+        )
+        
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return self.layers(x)
+    
+    
---
+++ GPT/Transformer.py
@@ -0,0 +1,34 @@
+import torch
+import torch.nn as nn
+from SubModules import LayerNorm, GelU, Linear
+from Attention import MultiHeadAttention
+
+class transformer(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+        self.att = MultiHeadAttention(
+            d_in = cfg['embed_dim'],
+            d_out= cfg['embed_dim'],
+            context_length= cfg['context_length'],
+            num_heads= cfg['n_heads'],
+            dropout= cfg['drop_rate'],
+        )
+        self.ff = Linear(cfg)
+        self.norm1 = LayerNorm(cfg['embed_dim'])
+        self.norm2 = LayerNorm(cfg['embed_dim'])
+        self.dropout = nn.Dropout(cfg['drop_rate'])
+        
+    def forward(self, x):
+        shortcut = x
+        x = self.norm1(x)
+        x = self.att(x)
+        x = self.dropout(x)
+        x = x + shortcut
+        shortcut = x
+        x = self.norm2(x)
+        x = self.ff(x)
+        x = self.norm2(x)
+        x = x + shortcut
+        return x
+        
+        
