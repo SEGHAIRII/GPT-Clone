b4b29355d3a8300e634a4c575e6d87b53c97d207
SEGHAIRII <la_seghairi@esi.dz>
2025-04-13 18:02:41 +0100

    implemented layer norm inorder to add shift and scale parameters

---
+++ GPT/LayerNorm.py
@@ -0,0 +1,17 @@
+import torch
+import torch.nn as nn
+
+class LayerNorm(nn.Module):
+    def __init__(self, dim: int, eps: float = 1e-5):
+        super().__init__()
+        self.eps = eps
+        self.dim = dim
+        self.scale = nn.Parameter(torch.ones(dim))
+        self.shift = nn.Parameter(torch.zeros(dim))
+
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        mean = x.mean(dim=-1, keepdim=True)
+        var = x.var(dim=-1, keepdim=True, unbiased=False)
+        norm_x = (x - mean) / torch.sqrt(var + self.eps)
+        return self.scale * norm_x + self.shift
---
+++ GPT/config.py
@@ -0,0 +1,8 @@
+GPT_config = {
+    'vocab_size':50257,
+    "context_length": 1024,
+    "emd_dim": 768,
+    "n_heads":12,
+    "n_layers":12,
+    "drop_rate":0.1,
+}
