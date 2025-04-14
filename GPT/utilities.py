d672047baa1d761ccf424b87c0cdfc1f8f185be6
SEGHAIRII <la_seghairi@esi.dz>
2025-04-14 21:53:18 +0100

    finalized the training

--- GPT/utilities.py
+++ GPT/utilities.py
@@ -2,16 +2,32 @@ import tiktoken
 import torch
 
 
-def generate_text(model, idx, max_new_tokens, context_size):
+def generate(model, idx, max_new_tokens, context_size,
+    temperature=0.0, top_k=None, eos_id=None):
     for _ in range(max_new_tokens):
         idx_cond = idx[:, -context_size:]
         with torch.no_grad():
             logits = model(idx_cond)
         logits = logits[:, -1, :]
-        idx_next = torch.argmax(logits, dim=-1, keepdim=True)
+
+         if top_k is not None:
+            top_logits, _ = torch.topk(logits, top_k)
+            min_val = top_logits[:, -1]
+            logits = torch.where(
+                logits < min_val,
+                torch.tensor(float('-inf')).to(logits.device),
+                logits
+            )
+        if temperature > 0.0:
+            logits = logits / temperature
+            probs = torch.softmax(logits, dim=-1)
+            idx_next = torch.multinomial(probs, num_samples=1)
+        else:
+            idx_next = torch.argmax(logits, dim=-1, keepdim=True)
+        if idx_next == eos_id:
+            break
         idx = torch.cat((idx, idx_next), dim=1)
-        
-    return idx
+    return id
 
 
 def text_to_token_ids(text, tokenizer):
@@ -29,6 +45,21 @@ def read_text(file_path:str = 'the-verdict.txt'):
     with open(file_path, 'r', encoding='utf-8') as file:
         text = file.read()
     return text
+
+
+
+def save_model_optimizer(self, model, optimizer):
+    torch.save({
+        'model_state_dict': model.state_dict(),
+        'optimizer_state_dict': optimizer.state_dict(),
+    }, "model_optim.pth")
+    
+    
+def load_model_optimizer(self, model, optimizer):
+    checkpoint = torch.load("model_optim.pth")
+    model.load_state_dict(checkpoint['model_state_dict'])
+    optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+    return model, optimizer
 
 
 
