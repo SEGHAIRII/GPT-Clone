906f4407c12f2a38f47b6f7d5989a14269d4dd15
SEGHAIRII <la_seghairi@esi.dz>
2025-04-14 11:25:53 +0100

    added utiliti functions

---
+++ GPT/utilities.py
@@ -0,0 +1,29 @@
+import tiktoken
+import torch
+
+
+def generate_text(model, idx, max_new_tokens, context_size):
+    for _ in range(max_new_tokens):
+        idx_cond = idx[:, -context_size:]
+        with torch.no_grad():
+            logits = model(idx_cond)
+        logits = logits[:, -1, :]
+        idx_next = torch.argmax(logits, dim=-1, keepdim=True)
+        idx = torch.cat((idx, idx_next), dim=1)
+        
+    return idx
+
+
+def text_to_token_ids(text, tokenizer):
+    encoded = tokenizer.encode(text, allowed_special = {'<|endoftext|>'})
+    encoded_tensor = torch.tensor(encoded).unsqueeze(0)
+    return encoded_tensor
+
+def token_ids_to_text(token_ids, tokenizer):
+    tokens = token_ids.squeeze(0)
+    return tokenizer.decode(tokens.tolist())
+
+
+    
+    
+        
